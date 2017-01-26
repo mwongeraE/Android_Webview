5eacf1565787763ca4c5e8a54e6e3586687d654f
mwongeraE <evansmwongera@gmail.com>
2017-01-26 23:33:26 +0300

    webview activity

--- app/src/main/java/com/mwongera/android_webview/WebViewActivity.java
+++ app/src/main/java/com/mwongera/android_webview/WebViewActivity.java
@@ -1,8 +1,21 @@
 package com.mwongera.android_webview;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
 /**
  * Created by mwongera on 1/26/17.
  */
 
-public class WebViewActivity {
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://docs.google.com/gview?embedded=true&url=uet.vnu.edu.vn/~chauttm/e-books/java/Effective.Java.2nd.Edition.May.2008.3000th.Release.pdf");
+    }
 }
